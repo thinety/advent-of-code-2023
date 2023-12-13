42e1d2c2b2dbf8f340cf749121e93d513cc45a55
Thiago Trannin <51510921+thinety@users.noreply.github.com>
2023-12-13 00:38:31 -0300

    Optimize day 05

--- src/day05/mod.rs
+++ src/day05/mod.rs
@@ -1,62 +1,72 @@
 mod parse;
 use parse::{parse_part1, parse_part2};
 
-use rayon::prelude::*;
-
-struct Mapping {
-    ranges: Vec<Range>,
-}
-
-impl Mapping {
-    fn map(&self, x: u64) -> u64 {
-        for range in &self.ranges {
-            let src = range.source;
-            let dst = range.destination;
-            let len = range.length;
-
-            if (src..src + len).contains(&x) {
-                return dst + (x - src);
-            }
-        }
-        x
-    }
-}
-
-struct Range {
-    source: u64,
-    destination: u64,
-    length: u64,
-}
-
 pub fn part1(input: &str) -> u64 {
     let (seeds, mappings) = parse_part1(input);
 
     seeds
         .iter()
-        .map(|&seed| mappings.iter().fold(seed, |x, mapping| mapping.map(x)))
+        .map(|&seed| {
+            mappings.iter().fold(seed, |x, mapping| {
+                for &(start, end, offset) in mapping {
+                    if (start..end).contains(&x) {
+                        return offset + (x - start);
+                    }
+                }
+                x
+            })
+        })
         .min()
         .unwrap()
 }
 
 pub fn part2(input: &str) -> u64 {
-    let (seeds, mappings) = parse_part2(input);
+    let (seeds, mut mappings) = parse_part2(input);
 
-    seeds
-        .iter()
-        .map(|&(start, length)| {
-            (start..start + length)
-                .into_par_iter()
-                .map(|seed| mappings.iter().fold(seed, |x, mapping| mapping.map(x)))
-                .min()
-                .unwrap()
-        })
-        .min()
-        .unwrap()
+    mappings.iter_mut().for_each(|mapping| mapping.sort());
+
+    let mut ranges = seeds;
+    for mapping in &mappings {
+        let mut new_ranges = Vec::new();
+
+        for &(mut start, end) in &ranges {
+            let mut last_map_end = 0;
+
+            for i in 0..mapping.len() {
+                let (map_start, map_end, map_offset) = mapping[i];
+
+                if last_map_end <= start && start < map_start {
+                    let new_start = end.min(map_start);
+                    new_ranges.push((start, new_start));
+                    start = new_start;
+                }
+
+                if map_start <= start && start < map_end {
+                    let new_start = end.min(map_end);
+                    new_ranges.push((
+                        map_offset + (start - map_start),
+                        map_offset + (new_start - map_start),
+                    ));
+                    start = new_start;
+                }
+
+                last_map_end = map_end;
+            }
+
+            if last_map_end <= start {
+                new_ranges.push((start, end));
+            }
+        }
+
+        ranges = new_ranges;
+    }
+
+    ranges.iter().map(|&(start, _)| start).min().unwrap()
 }
 
 crate::samples! {
     (part1_sample, part1, "sample.in", "35"),
     (part1_puzzle, part1, "puzzle.in", "486613012"),
     (part2_sample, part2, "sample.in", "46"),
-    //(part2_puzzle, part2, "puzzle.in", "56931769"),
+    (part2_puzzle, part2, "puzzle.in", "56931769"),
 }
--- src/day05/parse.rs
+++ src/day05/parse.rs
@@ -1,5 +1,3 @@
-use super::*;
-
 use nom::{
     bytes::complete::{tag, take_until1},
     character::complete::digit1,
@@ -9,36 +7,29 @@ use nom::{
     IResult,
 };
 
-pub(super) fn parse_part1(input: &str) -> (Vec<u64>, Vec<Mapping>) {
+pub(super) fn parse_part1(input: &str) -> (Vec<u64>, Vec<Vec<(u64, u64, u64)>>) {
     fn number(input: &str) -> IResult<&str, u64> {
         map_res(digit1, str::parse)(input)
     }
-    fn range(input: &str) -> IResult<&str, Range> {
+    fn range(input: &str) -> IResult<&str, (u64, u64, u64)> {
         map(
             tuple((number, tag(" "), number, tag(" "), number)),
-            |(destination, _, source, _, length)| Range {
-                source,
-                destination,
-                length,
-            },
+            |(destination, _, source, _, length)| (source, source + length, destination),
         )(input)
     }
-    fn ranges(input: &str) -> IResult<&str, Vec<Range>> {
+    fn ranges(input: &str) -> IResult<&str, Vec<(u64, u64, u64)>> {
         separated_list1(tag("\n"), range)(input)
     }
-    fn mapping(input: &str) -> IResult<&str, Mapping> {
-        map(
-            preceded(terminated(take_until1("\n"), tag("\n")), ranges),
-            |ranges| Mapping { ranges },
-        )(input)
+    fn mapping(input: &str) -> IResult<&str, Vec<(u64, u64, u64)>> {
+        preceded(terminated(take_until1("\n"), tag("\n")), ranges)(input)
     }
-    fn mappings(input: &str) -> IResult<&str, Vec<Mapping>> {
+    fn mappings(input: &str) -> IResult<&str, Vec<Vec<(u64, u64, u64)>>> {
         separated_list1(tag("\n\n"), mapping)(input)
     }
     fn seeds(input: &str) -> IResult<&str, Vec<u64>> {
         preceded(tag("seeds: "), separated_list1(tag(" "), number))(input)
     }
-    fn parse(input: &str) -> IResult<&str, (Vec<u64>, Vec<Mapping>)> {
+    fn parse(input: &str) -> IResult<&str, (Vec<u64>, Vec<Vec<(u64, u64, u64)>>)> {
         separated_pair(seeds, tag("\n\n"), mappings)(input)
     }
 
@@ -47,39 +38,35 @@ pub(super) fn parse_part1(input: &str) -> (Vec<u64>, Vec<Mapping>) {
     (seeds, mappings)
 }
 
-pub(super) fn parse_part2(input: &str) -> (Vec<(u64, u64)>, Vec<Mapping>) {
+pub(super) fn parse_part2(input: &str) -> (Vec<(u64, u64)>, Vec<Vec<(u64, u64, u64)>>) {
     fn number(input: &str) -> IResult<&str, u64> {
         map_res(digit1, str::parse)(input)
     }
-    fn range(input: &str) -> IResult<&str, Range> {
+    fn range(input: &str) -> IResult<&str, (u64, u64, u64)> {
         map(
             tuple((number, tag(" "), number, tag(" "), number)),
-            |(destination, _, source, _, length)| Range {
-                source,
-                destination,
-                length,
-            },
+            |(destination, _, source, _, length)| (source, source + length, destination),
         )(input)
     }
-    fn ranges(input: &str) -> IResult<&str, Vec<Range>> {
+    fn ranges(input: &str) -> IResult<&str, Vec<(u64, u64, u64)>> {
         separated_list1(tag("\n"), range)(input)
     }
-    fn mapping(input: &str) -> IResult<&str, Mapping> {
-        map(
-            preceded(terminated(take_until1("\n"), tag("\n")), ranges),
-            |ranges| Mapping { ranges },
-        )(input)
+    fn mapping(input: &str) -> IResult<&str, Vec<(u64, u64, u64)>> {
+        preceded(terminated(take_until1("\n"), tag("\n")), ranges)(input)
     }
-    fn mappings(input: &str) -> IResult<&str, Vec<Mapping>> {
+    fn mappings(input: &str) -> IResult<&str, Vec<Vec<(u64, u64, u64)>>> {
         separated_list1(tag("\n\n"), mapping)(input)
     }
-    fn seeds(input: &str) -> IResult<&str, Vec<(u64, u64)>> {
-        preceded(
-            tag("seeds: "),
-            separated_list1(tag(" "), separated_pair(number, tag(" "), number)),
+    fn seed(input: &str) -> IResult<&str, (u64, u64)> {
+        map(
+            separated_pair(number, tag(" "), number),
+            |(start, length)| (start, start + length),
         )(input)
     }
-    fn parse(input: &str) -> IResult<&str, (Vec<(u64, u64)>, Vec<Mapping>)> {
+    fn seeds(input: &str) -> IResult<&str, Vec<(u64, u64)>> {
+        preceded(tag("seeds: "), separated_list1(tag(" "), seed))(input)
+    }
+    fn parse(input: &str) -> IResult<&str, (Vec<(u64, u64)>, Vec<Vec<(u64, u64, u64)>>)> {
         separated_pair(seeds, tag("\n\n"), mappings)(input)
     }
 
