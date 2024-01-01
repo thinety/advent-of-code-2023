da56330393db6ab8ecd0afa2ddd8cac4d4c1451d
Thiago Trannin <51510921+thinety@users.noreply.github.com>
2024-01-01 17:18:28 -0300

    Optimize day 23

--- src/day23/mod.rs
+++ src/day23/mod.rs
@@ -2,75 +2,139 @@ fn parse(input: &str) -> Vec<Vec<u8>> {
     input.lines().map(|line| line.as_bytes().to_vec()).collect()
 }
 
-// 0 is start, 1 is end
-fn make_graph(grid: &Vec<Vec<u8>>) -> Vec<Vec<usize>> {
-    fn dfs(
-        (i, j): (usize, usize),
-        grid: &Vec<Vec<u8>>,
-        indexes: &mut Vec<Vec<Option<usize>>>,
-        neighbors: &mut Vec<Vec<usize>>,
-    ) {
-        let (n, m) = (grid.len(), grid[0].len());
-        let index = indexes[i][j].unwrap();
+fn neighbor_count((i, j): (usize, usize), grid: &Vec<Vec<u8>>) -> u32 {
+    let (n, m) = (grid.len(), grid[0].len());
 
-        let mut f = |ni: usize, nj: usize| {
-            if grid[ni][nj] == b'#' {
-                return;
-            }
+    let mut neighbor_count = 0;
 
-            let neighbor_index = indexes[ni][nj].unwrap_or_else(|| {
-                neighbors.push(Vec::new());
-                neighbors.len() - 1
-            });
-            neighbors[index].push(neighbor_index);
-
-            if indexes[ni][nj].is_none() {
-                indexes[ni][nj] = Some(neighbor_index);
-                dfs((ni, nj), grid, indexes, neighbors);
-            }
-        };
-        if (grid[i][j] == b'.' || grid[i][j] == b'^') && i > 0 {
-            f(i - 1, j);
-        }
-        if (grid[i][j] == b'.' || grid[i][j] == b'v') && i < n - 1 {
-            f(i + 1, j);
-        }
-        if (grid[i][j] == b'.' || grid[i][j] == b'<') && j > 0 {
-            f(i, j - 1);
-        }
-        if (grid[i][j] == b'.' || grid[i][j] == b'>') && j < m - 1 {
-            f(i, j + 1);
-        }
+    if i > 0 && grid[i - 1][j] != b'#' {
+        neighbor_count += 1;
+    }
+    if i < n - 1 && grid[i + 1][j] != b'#' {
+        neighbor_count += 1;
+    }
+    if j > 0 && grid[i][j - 1] != b'#' {
+        neighbor_count += 1;
+    }
+    if j < m - 1 && grid[i][j + 1] != b'#' {
+        neighbor_count += 1;
     }
 
-    let mut neighbors = vec![];
+    neighbor_count
+}
 
+// 0 is start, 1 is end
+fn make_graph(grid: &Vec<Vec<u8>>) -> Vec<Vec<(usize, u32)>> {
     let (n, m) = (grid.len(), grid[0].len());
-    let mut indexes = vec![vec![None; m]; n];
 
+    let mut relevant = Vec::new();
+    let mut index = vec![vec![None; m]; n];
+    let mut neighbors = Vec::new();
+
+    relevant.push((0, 1));
+    index[0][1] = Some(neighbors.len());
     neighbors.push(Vec::new());
-    indexes[0][1] = Some(neighbors.len() - 1);
 
+    relevant.push((n - 1, m - 2));
+    index[n - 1][m - 2] = Some(neighbors.len());
     neighbors.push(Vec::new());
-    indexes[n - 1][m - 2] = Some(neighbors.len() - 1);
 
-    dfs((0, 1), grid, &mut indexes, &mut neighbors);
-    dfs((n - 1, m - 2), grid, &mut indexes, &mut neighbors);
+    while let Some((i, j)) = relevant.pop() {
+        fn dfs(
+            (idx, i, j): (usize, usize, usize),
+            d: u32,
+            grid: &Vec<Vec<u8>>,
+            visited: &mut Vec<Vec<bool>>,
+            relevant: &mut Vec<(usize, usize)>,
+            index: &mut Vec<Vec<Option<usize>>>,
+            neighbors: &mut Vec<Vec<(usize, u32)>>,
+        ) {
+            let (n, m) = (grid.len(), grid[0].len());
+
+            let mut f = |ni: usize, nj: usize| {
+                if grid[ni][nj] == b'#' || visited[ni][nj] {
+                    return;
+                }
+                visited[ni][nj] = true;
+
+                if neighbor_count((ni, nj), grid) != 2 {
+                    assert!(grid[ni][nj] == b'.');
+
+                    if index[ni][nj].is_none() {
+                        relevant.push((ni, nj));
+                        index[ni][nj] = Some(neighbors.len());
+                        neighbors.push(Vec::new());
+                    }
+
+                    let neighbor_idx = index[ni][nj].unwrap();
+                    neighbors[idx].push((neighbor_idx, d + 1));
+                    return;
+                }
+
+                dfs(
+                    (idx, ni, nj),
+                    d + 1,
+                    grid,
+                    visited,
+                    relevant,
+                    index,
+                    neighbors,
+                );
+            };
+            if (grid[i][j] == b'.' || grid[i][j] == b'^') && i > 0 {
+                f(i - 1, j);
+            }
+            if (grid[i][j] == b'.' || grid[i][j] == b'v') && i < n - 1 {
+                f(i + 1, j);
+            }
+            if (grid[i][j] == b'.' || grid[i][j] == b'<') && j > 0 {
+                f(i, j - 1);
+            }
+            if (grid[i][j] == b'.' || grid[i][j] == b'>') && j < m - 1 {
+                f(i, j + 1);
+            }
+        }
+
+        let idx = index[i][j].unwrap();
+
+        let mut visited = vec![vec![false; m]; n];
+        visited[i][j] = true;
+
+        dfs(
+            (idx, i, j),
+            0,
+            grid,
+            &mut visited,
+            &mut relevant,
+            &mut index,
+            &mut neighbors,
+        );
+    }
 
     neighbors
 }
 
-fn dfs(i: usize, neighbors: &Vec<Vec<usize>>, distance: &mut Vec<Option<u32>>, ans: &mut u32) {
+fn dfs(
+    i: usize,
+    neighbors: &Vec<Vec<(usize, u32)>>,
+    visited: &mut Vec<bool>,
+    distance: &mut Vec<u32>,
+    ans: &mut u32,
+) {
+    let d = distance[i];
+
     if i == 1 {
-        *ans = (*ans).max(distance[i].unwrap());
+        *ans = (*ans).max(d);
     }
 
-    for &ni in neighbors[i].iter() {
-        if distance[ni].is_none() {
-            distance[ni] = Some(distance[i].unwrap() + 1);
-            dfs(ni, neighbors, distance, ans);
-            distance[ni] = None;
+    for &(ni, ec) in neighbors[i].iter() {
+        if visited[ni] {
+            continue;
         }
+        visited[ni] = true;
+        distance[ni] = d + ec;
+        dfs(ni, neighbors, visited, distance, ans);
+        visited[ni] = false;
     }
 }
 
@@ -81,14 +145,20 @@ pub fn part1(input: &str) -> u32 {
     let n = neighbors.len();
 
     let mut ans = 0;
-    let mut distance = vec![None; n];
-    distance[0] = Some(0);
-    dfs(0, &neighbors, &mut distance, &mut ans);
+
+    let mut visited = vec![false; n];
+    let mut distance = vec![0; n];
+
+    visited[0] = true;
+    distance[0] = 0;
+    dfs(0, &neighbors, &mut visited, &mut distance, &mut ans);
+
     ans
 }
 
 pub fn part2(input: &str) -> u32 {
     let mut grid = parse(input);
+
     let (n, m) = (grid.len(), grid[0].len());
     for i in 0..n {
         for j in 0..m {
@@ -102,9 +172,14 @@ pub fn part2(input: &str) -> u32 {
     let n = neighbors.len();
 
     let mut ans = 0;
-    let mut distance = vec![None; n];
-    distance[0] = Some(0);
-    dfs(0, &neighbors, &mut distance, &mut ans);
+
+    let mut visited = vec![false; n];
+    let mut distance = vec![0; n];
+
+    visited[0] = true;
+    distance[0] = 0;
+    dfs(0, &neighbors, &mut visited, &mut distance, &mut ans);
+
     ans
 }
 
